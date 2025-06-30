3f54a68cfccb47cefe1aae7be1b80a65848949b0
dsalfonso0310 <desariealfonso@gmail.com>
2025-06-30 21:12:30 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'db.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Multi-System Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+    <h1>Multi-System Dashboard</h1>
+    <nav>
+        <a href="library/view_library.php">📚 Library</a>
+        <a href="attendance/view_attendance.php">🧑‍🎓 Attendance</a>
+        <a href="movie/view_movie.php">🎬 Movies</a>
+        <a href="timelog/view_timelog.php">🕒 Timelogs</a>
+    </nav>
+</div>
+</body>
+</html>
